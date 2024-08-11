19b06edd781606a6488e806b8b344a4a5de08b5a
MoepyaCode <kagisomoepya19@gmail.com>
2024-08-11 23:27:52 +0200

    🎉 Created setup of article-preview-component

---
+++ src/assets/index.ts
@@ -0,0 +1,15 @@
+import avatarMichelle from './avatar-michelle.jpg';
+import drawers from './drawers.jpg';
+import iconFacebook from './icon-facebook.svg';
+import iconPinterest from './icon-pinterest.svg';
+import iconSahre from './icon-share.svg';
+import iconTwitter from './icon-twitter.svg';
+
+export const Assets = {
+    avatarMichelle,
+    drawers,
+    iconFacebook,
+    iconPinterest,
+    iconSahre,
+    iconTwitter,
+}
